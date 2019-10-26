fc914bf0ba0eaf7d1ec897fb8c16bcadd534456c
Bao <bghoang.ucsd.edu>
2019-10-26 05:03:06 -0700

    props thingy

---
+++ goal_app/src/components/MainBlock/tagInfor.jsx
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: "Test1",
+    photo: "src"
+  },
+  {
+    name: "Test2",
+    photo: "src"
+  },
+  {
+    name: "Test3",
+    photo: "src"
+  },
+  {
+    name: "Test4",
+    photo: "src"
+  },
+  {
+    name: "Test5",
+    photo: "src"
+  }
+];
